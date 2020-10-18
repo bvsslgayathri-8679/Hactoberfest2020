b1a1c1ed56da7720c3a5a044f33b70589a548500
B Gayathri <69475106+bvsslgayathri-8679@users.noreply.github.com>
2020-10-18 12:37:40 +0530

    Towers of honai in cpp

--- tower_of_hanoi.c
+++ tower_of_hanoi.c
@@ -7,8 +7,7 @@ void towerOfHanoi(int d,char T1,char T2,char T3)
 	}
 	towerOfHanoi(d-1,T1,T3,T2);
 	printf("\nmove disk from %c to %c",T1,T2);
-	towerOfHanoi(d-1,T3,T2,T1);
-	
+	towerOfHanoi(d-1,T3,T2,T1);	
 }
 
 int main(){
